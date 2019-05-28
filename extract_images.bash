bf33b6426716493399905977a35988c1d43f6afc
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-05-28 15:56:47 +0200

    make call to export_jpeg to export images

--- extract_images.bash
+++ extract_images.bash
@@ -1,17 +1,20 @@
-!#/bin/bash
+#!/bin/bash
 
 date='190426'
 file='2019-04-26-10-34-00.bag'
 location='witzwil1'
+camera='Photonfocus_vis'
+
+path_in="/media/$USER/Samsung_2TB/Datasets/$date/$location/$file"
+path_out="/media/$USER/Samsung_2TB/Processed/$date/$location/$camera"
+
+echo "Creating directory $path_out"
+mkdir -p $path_out
+
+export ROS_HOME=$path_out
+
+roslaunch photonfocus_camera export_jpeg.launch
 
 
-path=/media/$USER/'Samsung 2TB'/Datasets/$date/$location/$file
-
-
-
-
-
-
-roscore
-rosrun image_view extract_images _sec_per_frame:=0.01 image:=/ssf/BFS_usb_0/image_raw
-rosbag play 2019-04-18-14-01-03.bag
+# rosrun image_view extract_images _sec_per_frame:=0.01 image:=/ssf/BFS_usb_0/image_raw
+# rosbag play 2019-04-18-14-01-03.bag

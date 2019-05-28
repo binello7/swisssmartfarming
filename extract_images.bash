20713c901124e57d0bad0f376154e1163d4a843d
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-05-28 09:17:14 +0200

    add file

--- extract_images.bash
+++ extract_images.bash
@@ -1,3 +1,17 @@
+!#/bin/bash
+
+date='190426'
+file='2019-04-26-10-34-00.bag'
+location='witzwil1'
+
+
+path=/media/$USER/'Samsung 2TB'/Datasets/$date/$location/$file
+
+
+
+
+
+
 roscore
 rosrun image_view extract_images _sec_per_frame:=0.01 image:=/ssf/BFS_usb_0/image_raw
 rosbag play 2019-04-18-14-01-03.bag

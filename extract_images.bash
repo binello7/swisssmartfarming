6d52f469d38337466313184289ced2847cf102da
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-05-29 17:06:06 +0200

    add cameras and corresponding topics

--- extract_images.bash
+++ extract_images.bash
@@ -4,7 +4,17 @@ date='190426'
 file='2019-04-26-10-34-00.bag'
 location='witzwil1'
 camera='Photonfocus_vis'
+cameras=(
+  BFS
+  Photonfocus_vis
+  Ximea
+)
 topic='/ssf/photonfocus_camera_vis_node/image_raw'
+topics=(
+  /ssf/BFS_usb_0/image_raw
+  /ssf/photonfocus_camera_vis_node/image_raw
+  /ximea_asl/image_raw
+)
 
 path_in="/media/$USER/Samsung_2TB/Datasets/$date/$location/$file"
 path_out="/media/$USER/Samsung_2TB/Processed/$date/$location/$camera"

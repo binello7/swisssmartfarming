a846bfbb0911395828111cbd1dc1cb7bc275ef2d
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-05-30 10:46:13 +0200

    loop through all topics and export images to specified folder

--- extract_images.bash
+++ extract_images.bash
@@ -3,6 +3,7 @@
 date='190426'
 file='2019-04-26-10-34-00.bag'
 location='witzwil1'
+
 camera='Photonfocus_vis'
 cameras=(
   BFS
@@ -17,15 +18,11 @@ topics=(
 )
 
 path_in="/media/$USER/Samsung_2TB/Datasets/$date/$location/$file"
-path_out="/media/$USER/Samsung_2TB/Processed/$date/$location/$camera"
 
-
-
-echo "Creating directory $path_out"
-mkdir -p $path_out
-
-export ROS_HOME=$path_out
-
-echo $path_in
-echo $topic
-roslaunch mav_startup export_jpeg.launch path:=$path_in topic:=$topic
+for ((i=0; i<${#cameras[@]}; i++)); do
+  path_out="/media/$USER/Samsung_2TB/Processed/$date/$location/${cameras[$i]}"
+  echo "Extract images to folder $path_out"
+  mkdir -p $path_out
+  export ROS_HOME=$path_out
+  roslaunch mav_startup export_jpeg.launch path:=$path_in topic:=${topics[$i]} &
+done

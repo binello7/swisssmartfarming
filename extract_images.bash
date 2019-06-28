03866cd54242061c2a8fe014bd97d1f1514523ab
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-06-28 14:35:49 +0200

    add number of bands per each camera

--- extract_images.bash
+++ extract_images.bash
@@ -11,6 +11,13 @@ cameras=(
   #Ximea
 )
 
+bands=(
+  3
+  16
+  25
+  #25
+)
+
 topics=(
   /ssf/BFS_usb_0/image_raw
   /ssf/photonfocus_camera_vis_node/image_raw
@@ -21,16 +28,22 @@ topics=(
 path_bag="/media/$USER/Samsung_2TB/Datasets/$date/$location/$file"
 path_location="/media/$USER/Samsung_2TB/Processed/$date/$location"
 
+# Write rtk-GPS data to csv-file
+echo "Saving rtk-GPS data..."
+rostopic echo -b $path_bag -p /ssf/dji_sdk/rtk_position > "$path_location/rtk_data.csv"
+
 for ((i=0; i<${#cameras[@]}; i++)); do
   path_camera="$path_location/${cameras[$i]}"
   echo "Save images to folder $path_camera"
-  mkdir -p $path_camera
-  python bag2img.py --img_topic=${topics[$i]} --bag=$path_bag \
-    --output_folder=$path_camera --output_format=jpg
+  # mkdir -p $path_camera
+  # python bag2img.py --img_topic=${topics[$i]} --bag=$path_bag \
+  #   --output_folder=$path_camera --output_format=jpg
+  if [ ${cameras[$i]} == "Ximea" ] || [ ${cameras[$i]} == "Photonfocus_vis" ] || [ ${cameras[$i]} == "Photonfocus_nir" ]
+  then
+    python resample_mosaics.py --input_folder=$path_camera --nb_bands=${bands[$i]}
+  fi
 
   # Write images timestamps to file
+  echo "Saving images timestamps..."
   rostopic echo -b $path_bag -p "${topics[$i]}/header" > "$path_camera/img_tstamps.csv"
 done
-
-# Write rtk-GPS data to csv-file
-rostopic echo -b $path_bag -p /ssf/dji_sdk/rtk_position > "$path_location/rtk_data.csv"

ddb547e383bc24c11fb0733a32c39e988d3eba12
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-06-28 14:39:39 +0200

    refactor code to use program from command line with options

--- rtk2exif.py
+++ rtk2exif.py
@@ -2,47 +2,57 @@
 
 import numpy as np
 import os
+import argparse
 
-# program inputs
-project_folder = "/home/seba/Desktop/20190418/"
-images_parentfolder = "xi_res2_??/"
-bag_file = "2019-04-18-14-01-03.bag"
-rtk_file = "140103_rtk_position.csv"
-stamps_file = "140103_stamps.txt"
-images_topic = "/ximea_asl/image_raw/header/stamp"
-bands = 25
+class Rtk_writer(object):
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description='writes rtk-data to extracted images')
+        self.args = None
+        self.args_parse()
+        self.rtk_file = None
+        self.tstamps_file = None
+        self.img_folder = None
 
-rtk_data = np.genfromtxt((project_folder + rtk_file), delimiter=',', skip_header=1)
+    def args_parse(self):
+        self.parser.add_argument('--input_folder', required=True,
+                        metavar="/my/path/to/input/images",
+                        help='Path to the folder where images are stored')
+        self.parser.add_argument('--rtk_file', required=True,
+                        help='Path to the rtk-data file')
+        self.parser.add_argument('--tstamps_file', required=True,
+                        help='Path to the timestamps-data file')
+        self.args = self.parser.parse_args()
 
-stamps_data = open((project_folder + stamps_file)).read()
+    def run(self):
+        self.rtk_file = self.args.rtk_file
+        self.tstamps_file = self.args.tstamps_file
+        self.img_folder = self.args.input_folder
 
-# store the frames' timestamps into an array
-secs_nsecs = [int(s) for s in stamps_data.split() if s.isdigit()]
-tstamps = np.zeros([int(len(secs_nsecs) / 2), 1])
-for t in range(int(len(secs_nsecs) / 2)):
-    tstamp = str(secs_nsecs[2 * t]) + '.' + str(secs_nsecs[2 * t + 1])
-    tstamps[t] = float(tstamp, )
+        rtk_data = np.genfromtxt(self.rtk_file, delimiter=',', skip_header=1)
+        tstamps_data = np.genfromtxt(self.tstamps_file, delimiter=',', skip_header=1)
 
-tstamps = tstamps * 1E9
+        lat = np.zeros(tstamps_data.shape[0])
+        lon = np.zeros(tstamps_data.shape[0])
+        alt = np.zeros(tstamps_data.shape[0])
 
-# interpolate lat/lon for every frame
-lat = np.interp(tstamps, rtk_data[:, 2], rtk_data[:, 6])
-lon = np.interp(tstamps, rtk_data[:, 2], rtk_data[:, 7])
-alt = np.interp(tstamps, rtk_data[:, 2], rtk_data[:, 8])
+        # interpolate lat/lon/alt for every frame
+        lat = np.interp(tstamps_data[:,2], rtk_data[:, 2], rtk_data[:, 6])
+        lon = np.interp(tstamps_data[:,2], rtk_data[:, 2], rtk_data[:, 7])
+        alt = np.interp(tstamps_data[:,2], rtk_data[:, 2], rtk_data[:, 8])
 
-# write exif metadata to frames
-img_files = sorted(os.listdir((project_folder + 'xi_res2_01/')))
-for i in range(len(tstamps)):
-    img_file = img_files[i].split('_')[0] + '*'
-    os.system("exiftool -GPSLatitudeRef=%.1f %s%s%s  -overwrite_original" %
-              (lat[i], project_folder, images_parentfolder, img_file))
-    os.system("exiftool -GPSLatitude=%.10f %s%s%s -overwrite_original" %
-              (lat[i], project_folder, images_parentfolder, img_file))
-    os.system("exiftool -GPSLongitudeRef=%.1f %s%s%s -overwrite_original" %
-              (lon[i], project_folder, images_parentfolder, img_file))
-    os.system("exiftool -GPSLongitude=%.10f %s%s%s -overwrite_original" %
-              (lon[i], project_folder, images_parentfolder, img_file))
-#    os.system('exiftool -GPSAltitudeRef="Above Sea Level" %s%s%s -overwrite_original' %
-#              (project_folder, images_parentfolder, img_file))
-#    os.system("exiftool -GPSAltitude=%.10f %s%s%s -overwrite_original" %
-#              (alt[i], project_folder, images_parentfolder, img_file))
+        # write exif metadata to frames
+        img_names = sorted(os.listdir(self.img_folder))
+        for i in range(len(img_names)):
+            if img_names[i].startswith('frame_'):
+                os.system("exiftool -GPSLatitudeRef=%.1f %s  -overwrite_original" %
+                          (lat[i], os.path.join(self.img_folder, img_names[i])))
+                os.system("exiftool -GPSLatitude=%.10f %s -overwrite_original" %
+                          (lat[i], os.path.join(self.img_folder, img_names[i])))
+                os.system("exiftool -GPSLongitudeRef=%.1f %s -overwrite_original" %
+                          (lon[i], os.path.join(self.img_folder, img_names[i])))
+                os.system("exiftool -GPSLongitude=%.10f %s -overwrite_original" %
+                          (lon[i], os.path.join(self.img_folder, img_names[i])))
+
+if __name__ == "__main__":
+    rtk2exif = Rtk_writer()
+    rtk2exif.run()

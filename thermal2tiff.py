f0e3dcc38e499e9e6b7c28fe56adb843f19b332a
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-08-12 18:01:29 +0200

    use the argparse module to parse optional arguments when calling running the script

--- thermal2tiff.py
+++ thermal2tiff.py
@@ -1,42 +1,40 @@
+#!/usr/bin/python2
+
 import subprocess, yaml
 import rosbag
 from cv_bridge import CvBridge
 import cv2
 import matplotlib.pyplot as plt
+from PIL import Image
+import os
+import argparse
 
-bag_file = "/media/seba/Samsung_2TB/Datasets/gorner_glacier/190709/2019-07-09-21-19-08.bag"
-read_topic = "/ssf/thermalgrabber_ros/image_deg_celsius"
-outputPath = "/home/seba/Desktop/Thermal"
+if __name__ == "__main__":
 
-bag = rosbag.Bag(bag_file)
-info_dict = yaml.load(subprocess.Popen(['rosbag', 'info', '--yaml', bag_file], stdout=subprocess.PIPE).communicate()[0])
-print(info_dict)
+    parser = argparse.ArgumentParser(description='Extract thermal images messages and saves them in .tiff format')
+    parser.add_argument('--bag_file',
+                            required=True,
+                            help='Path to the folder containing the mosaic images to resample')
+    parser.add_argument('--output_folder',
+                            required=True,
+                            help='Number of bands of the camera')
+    args = parser.parse_args()
 
+    topic = '/ssf/thermalgrabber_ros/image_deg_celsius'
 
-# print(bag)
+    bag = rosbag.Bag(args.bag_file)
+    info_dict = yaml.load(subprocess.Popen(['rosbag', 'info', '--yaml', args.bag_file], stdout=subprocess.PIPE).communicate()[0])
+    print(info_dict)
 
-genBag = bag.read_messages(read_topic)
-tmax = 0
-tmin = 0
+    genBag = bag.read_messages(topic)
 
-for k,b in enumerate(genBag):
-    print("OK, %d / %d" % (k, info_dict['messages']))
+    for k,b in enumerate(genBag):
+        print('saving file {} / {}'.format(k, info_dict['messages']))
 
-    cb = CvBridge()
-    cv_image = cb.imgmsg_to_cv2(b.message, b.message.encoding)
+        cb = CvBridge()
+        cv_image = cb.imgmsg_to_cv2(b.message, b.message.encoding)
 
-    data = cv_image[:,:]
-    # if data.min() < tmin:
-    #     tmin = data.min()
-    #
-    # if data.max() > tmax:
-    #     tmax = data.max()
-    #
-    # print(tmax)
-    # print(tmin)
-    #
-    # fig, ax = plt.subplots()
-    # img = ax.imshow(data, cmap='magma')
-    # fig.colorbar(img)
-    # plt.savefig(outputPath + '/' + str(k) + '.jpg')
-    # plt.close(fig)
+        data = cv_image[:,:]
+
+        img = Image.fromarray(data)
+        img.save(os.path.join(args.output_folder, 'frame_{:06}.tiff'.format(k)))

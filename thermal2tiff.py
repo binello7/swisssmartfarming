fc4c7c50e274e173692104f0b60fcd472ac6e46b
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-08-15 11:43:22 +0200

    get the number of messages for Tau2 instead of global for the bag

--- thermal2tiff.py
+++ thermal2tiff.py
@@ -11,6 +11,7 @@ import argparse
 
 if __name__ == "__main__":
 
+    # parse optional arguments
     parser = argparse.ArgumentParser(description='Extract thermal images messages and saves them in .tiff format')
     parser.add_argument('--bag_file',
                             required=True,
@@ -20,16 +21,21 @@ if __name__ == "__main__":
                             help='Number of bands of the camera')
     args = parser.parse_args()
 
+    # define the topic we want to extract
     topic = '/ssf/thermalgrabber_ros/image_deg_celsius'
 
+    # open the bag file
     bag = rosbag.Bag(args.bag_file)
     info_dict = yaml.load(subprocess.Popen(['rosbag', 'info', '--yaml', args.bag_file], stdout=subprocess.PIPE).communicate()[0])
-    print(info_dict)
+
+    # get the number of messages for the chosen topic
+    tau_info = filter(lambda tau: tau['topic'] == topic, info_dict['topics'])
+    n_msgs = tau_info[0]['messages']
 
     genBag = bag.read_messages(topic)
 
-    for k,b in enumerate(genBag):
-        print('saving file {} / {}'.format(k, info_dict['messages']))
+    for k, b in enumerate(genBag):
+        print('saving file {} / {}'.format(k+1, n_msgs))
 
         cb = CvBridge()
         cv_image = cb.imgmsg_to_cv2(b.message, b.message.encoding)

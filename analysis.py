a3f29e7b6396d9c98a9b84b5974c268e4476c0cf
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-10-16 18:21:01 +0200

    use skimage to identify local maxima

--- analysis.py
+++ analysis.py
@@ -4,25 +4,96 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ssf_functions as ssf
 from osgeo import gdal
+#-------------------------------------------------------------------------------
+from scipy import ndimage as ndi
+from skimage.feature import peak_local_max
 
-bare_path = "/media/seba/Samsung_2TB/Analysis/niederhasli_bare_field_rgb_dsm_crop.tif"
-plants_path = "/media/seba/Samsung_2TB/Analysis/niederhasli_20191007_rgb_dsm_crop.tif"
+px_res = 0.00814116 # m/px
 
-bare_array = ssf.read_geotiff(bare_path)
-plants_array = ssf.read_geotiff(plants_path)
+date1_path = "/media/seba/Samsung_2TB/Analysis/QGIS/niederhasli_mosaic.qgs/dsm/niederhasli_20190527_rgb_dsm_C9.tif"
+date2_path = "/media/seba/Samsung_2TB/Analysis/QGIS/niederhasli_mosaic.qgs/dsm/niederhasli_20190719_rgb_dsm_C9.tif"
+date3_path = "/media/seba/Samsung_2TB/Analysis/QGIS/niederhasli_mosaic.qgs/dsm/niederhasli_20191007_rgb_dsm_C9.tif"
 
-min_diff = np.min(bare_array[bare_array!=0]) - np.min(plants_array[plants_array!=0])
+date1_array = ssf.read_geotiff(date1_path)
+date2_array = ssf.read_geotiff(date2_path)
+date3_array = ssf.read_geotiff(date3_path)
 
-bare_array = bare_array - min_diff
-plants_array[1176:1257,4592:4663] = bare_array[1176:1257,4592:4663]
+# obtain field mask
+field_mask = date1_array==0
 
-diff_array = plants_array - bare_array
+# compute relative height differences from absolute altitudes
+date1_array = date1_array - np.min(date1_array[date1_array!=0])
+date2_array = date2_array - np.min(date2_array[date2_array!=0])
+date3_array = date3_array - np.min(date3_array[date3_array!=0])
+
+# flatten the terrain by removing the "DSM before sowing"
+date2_array = date2_array - date1_array
+date3_array = date3_array - date1_array
+
+date2_orig = date2_array.copy()
+date3_orig = date3_array.copy()
+
+
+date3_array[date3_array<0.3] = 0
+
+# try scipy
+image_max = ndi.maximum_filter(date3_array, size=100, mode='constant')
+coordinates = peak_local_max(date3_array, min_distance=100)
+print(coordinates.shape)
+#-------------------------------------------------------------------------------
+
+fig, axs = plt.subplots(1, 3)
+axs[0].imshow(date3_array)
+axs[0].set_title('date3_array')
+
+axs[1].imshow(image_max)
+axs[1].set_title('image_max')
+
+axs[2].imshow(date3_array)
+axs[2].autoscale(False)
+axs[2].plot(coordinates[:, 1], coordinates[:, 0], 'r.')
+plt.show()
 
 
 
-# bare_array = bare_array - min_diff
+# plt.imshow(date3_array)
+# plt.colorbar()
+# plt.show()
+
+
+# from scipy import ndimage as ndi
+# from skimage.feature import peak_local_max
+# from skimage import data, img_as_float
 #
-# max_diff = np.max(plants_array) - np.max(bare_array)
+#
+# # image_max is the dilation of im with a 20*20 structuring element
+# # It is used within peak_local_max function
+# image_max = ndi.maximum_filter(im, size=20, mode='constant')
+#
+# # Comparison between image_max and im to find the coordinates of local maxima
+# coordinates = peak_local_max(im, min_distance=20)
+#
+# # display results
+# fig, axes = plt.subplots(1, 3, figsize=(8, 3), sharex=True, sharey=True)
+# ax = axes.ravel()
+# ax[0].imshow(im, cmap=plt.cm.gray)
+# ax[0].axis('off')
+# ax[0].set_title('Original')
+#
+# ax[1].imshow(image_max, cmap=plt.cm.gray)
+# ax[1].axis('off')
+# ax[1].set_title('Maximum filter')
+#
+# ax[2].imshow(im, cmap=plt.cm.gray)
+# ax[2].autoscale(False)
+# ax[2].plot(coordinates[:, 1], coordinates[:, 0], 'r.')
+# ax[2].axis('off')
+# ax[2].set_title('Peak local max')
+#
+# fig.tight_layout()
+#
+# plt.show()
+
 
 
 

f13e795d6888d454b787a233e1fbf6586cfe8056
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-10-30 18:40:37 +0100

    add documentation for gdal_crop function

--- ssf_functions.py
+++ ssf_functions.py
@@ -8,6 +8,7 @@ from roipoly import RoiPoly
 from matplotlib import pyplot as plt
 import ssf_functions as ssf
 import math
+import os
 
 def read_geotiff(filepath):
     img_array = gdal_array.LoadFile(filepath)
@@ -64,3 +65,45 @@ def argmax_2D(array_2D):
     idx_1 = math.trunc(flat_idx / array_2D.shape[1])
     idx_2 = np.argmax(array_2D[idx_1,:])
     return [idx_1, idx_2]
+
+
+def gdal_crop(input_file, cutline_file, Xres, Yres,
+              output_file, nodata_val=0.0):
+    """function wrapping the GDAL function 'gdalwarp'.
+
+    Makes a call of the 'gdalwarp' function with the passed parameters.
+    For this to be executed correctly GDAL has to be correctly installed
+    on the system.
+
+    Parameters
+    ----------
+    input_file : str
+        path to the file to be cropped
+    cutline_file : str
+        path to the file to be used as cutline
+    Xres : float
+        desired output X-resolution
+    Yres : float
+        desired output Y-resolution
+    output_file : str
+        path and name of the output file
+    nodata_val : float, optional
+        pixel value to assign to regions outside the cutline. If no
+        value is passed, then this is set to 0
+
+    """
+
+    cutline_name = cutline_file.split("/")[-1].split(".")[0]
+    sys_cmd = ("gdalwarp -of GTiff -tr {} {} -tap -cutline"
+               " {} -cl {} -crop_to_cutline -dstnodata {} {} {}").format(
+                    Xres,
+                    Yres,
+                    cutline_file,
+                    cutline_name,
+                    nodata_val,
+                    input_file,
+                    output_file
+                )
+    print(nodata_val)
+    print(type(nodata_val))
+    os.system(sys_cmd)

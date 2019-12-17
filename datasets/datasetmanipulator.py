f535f306c8dde8d0d66da58a6344afd1603c79c8
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-12-17 14:57:33 +0100

    add 'pad_geotiff_from_grid' method

--- datasets/datasetmanipulator.py
+++ datasets/datasetmanipulator.py
@@ -1,37 +1,46 @@
 import rasterio as rio
+from rasterio.transform import Affine
 import numpy as np
 import geopandas as gpd
 from shapely.geometry import Polygon
+import os
 
 from IPython import embed
 
 class DatasetManipulator:
-    def __init__(self, geotiff_path, dataset_name):
-        self.dataset = rio.open(geotiff_path)
-        self.dataset_name = dataset_name
+    def __init__(self, dataset_path):
+        self.dataset_path = dataset_path
+        self.dataset_path_padded = None
+        self.dataset_name = "_".join(dataset_path.split("/")[-1].split("_")[:3])
+        self.dataset = rio.open(dataset_path)
         self.transform = self.dataset.transform
-        self.Xres = self.dataset.transform[0]
-        self.Yres = -self.dataset.transform[4]
-        self.x_spacing = None
-        self.y_spacing = None
+        self.crs = self.dataset.crs
+        self.Xres = self.transform[0]
+        self.Yres = -self.transform[4]
+        self.gridspacing_x = None
+        self.gridspacing_y = None
         self.grid = None
+        self.grid_bounds = None
 
-    def set_grid(self, outer_shapefile, x_spacing=256, y_spacing=256):
+
+    # def load_geotiff():
+
+    def set_grid(self, outer_shapefile, gridspacing_x=256, gridspacing_y=256):
         """Creates a grid and sets it to the grid dataset attribute.
 
         Given the shapefile confining the dataset geographic area , creates the geometry of a
         grid covering it. If the grid does not fit exactly in the shapefile, the grid
         will have an extra cell in order to cover all of the area. The x- and y
-        cell spacings of the grid are determined by the parameters <x_spacing> and
-        <y_spacing> which have to be given in pixels.
+        cell spacings of the grid are determined by the parameters <gridspacing_x> and
+        <gridspacing_y> which have to be given in pixels.
 
         Parameters
         ----------
         outer_shapefile: geopandas.geodataframe.GeoDataFrame
             shapefile confining the area
-        x_spacing: int
+        gridspacing_x: int
             cell width in pixels
-        y_spacing: int
+        gridspacing_y: int
             cell height in pixels
         """
 
@@ -39,41 +48,91 @@ class DatasetManipulator:
         xmin, ymin, xmax, ymax = outer_shapefile.geometry.total_bounds
 
         # set the x and y-spacing attributes
-        self.x_spacing = x_spacing
-        self.y_spacing = y_spacing
+        self.gridspacing_x = gridspacing_x
+        self.gridspacing_y = gridspacing_y
 
         # convert the cell-dimensions from px to units
-        x_spacing = x_spacing * self.Xres
-        y_spacing = y_spacing * self.Yres
+        gridspacing_x = gridspacing_x * self.Xres
+        gridspacing_y = gridspacing_y * self.Yres
 
         # get x and y number of cells
-        nx = (xmax - xmin) // x_spacing # //: integer division
-        if (xmax - xmin) % x_spacing != 0:
+        nx = (xmax - xmin) // gridspacing_x # //: integer division
+        if (xmax - xmin) % gridspacing_x != 0:
             nx = nx + 1
 
-        ny = (ymax - ymin) // y_spacing
-        if (ymax - ymin) % y_spacing != 0:
+        ny = (ymax - ymin) // gridspacing_y
+        if (ymax - ymin) % gridspacing_y != 0:
             ny = ny + 1
 
-        # get the new xmax, ymax
-        xmax = xmin + nx * x_spacing
-        ymax = ymin + ny * y_spacing
+        # get the new xmax, ymin
+        xmax = xmin + nx * gridspacing_x
+        ymin = ymax - ny * gridspacing_y
+
+        # set the grid bounds
+        self.grid_bounds = (xmin, ymin, xmax, ymax)
+
 
         # get the x and y coordinates of the grid
-        x_coord = list(np.arange(xmin, xmax, x_spacing))
-        y_coord = list(np.arange(ymin, ymax, y_spacing))
+        x_coord = list(np.arange(xmin, xmax, gridspacing_x))
+        y_coord = list(np.arange(ymin, ymax, gridspacing_y))
+        y_coord.reverse()
 
         # generate the polygon object determined by the 4 corners of each cell
         polygons = []
         for y in y_coord[:-1]:
             for x in x_coord[:-1]:
                 polygons.append(Polygon([(x, y),
-                                         (x+x_spacing, y),
-                                         (x+x_spacing, y+y_spacing),
-                                         (x, y+y_spacing)]))
+                                         (x+gridspacing_x, y),
+                                         (x+gridspacing_x, y-gridspacing_y),
+                                         (x, y-gridspacing_y)]))
 
         self.grid = gpd.GeoDataFrame({'geometry': polygons,
                                       'grid_idx': range(0, len(polygons))})
+
+
+    def pad_geotiff_from_grid(self):
+        if self.grid is None:
+            raise NotImplementedError('Grid not created yet')
+
+        # get the total height and width in pixels of the dataset after padding
+        tot_px_x = int(round((self.grid_bounds[2] - self.grid_bounds[0])
+            / self.Xres))
+        tot_px_y = int(round((self.grid_bounds[3] - self.grid_bounds[1])
+            / self.Yres))
+
+        # load the data for every band and shape them as (height, width, band)
+        array = self.dataset.read()
+
+        # for rgb do not consider alpha channel
+        if 'rgb' in self.dataset_name:
+            array = array[:-1,:,:]
+
+        pad_hor = tot_px_x - array.shape[2]
+        pad_ver = tot_px_y - array.shape[1]
+
+        array = np.pad(array, ((0, 0), (0, pad_ver), (0, pad_hor)),
+            mode='constant', constant_values=0)
+
+        self.dataset_path_padded = (os.path.join('/'
+            .join(self.dataset_path.split('/')[:-1]),
+            self.dataset_path.split('/')[-1].split('.')[0])
+            + '_padded.tif')
+
+        self.dataset = rio.open(
+            self.dataset_path_padded,
+            'w',
+            driver='Gtiff',
+            height=array.shape[1],
+            width=array.shape[2],
+            count=array.shape[0],
+            dtype=array.dtype,
+            crs=self.crs,
+            transform=self.transform
+        )
+
+        self.dataset.write(array, (1,2,3))
+        self.dataset.close()
+        self.dataset = rio.open(self.dataset_path_padded)
 
 
     def get_img_from_boundary(self, boundary):
@@ -109,7 +168,7 @@ class DatasetManipulator:
         col_start = int(col_start); col_end = int(col_end)
 
         # get the values of the pixel within the boundary
-        img = self.dataset.read()[:-1, row_start:row_end, col_start:col_end]
+        img = self.dataset.read()[:, row_start:row_end, col_start:col_end]
 
         # convert the image to channels last and return it
         return np.moveaxis(img, 0, 2)

36633a6d37254b86e55e70a4be99ef1254fb0a8d
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-02-05 18:13:16 +0100

    Add class HypSpecImage and xml-parsing functions.

--- hyperspec/hyperspec.py
+++ hyperspec/hyperspec.py
@@ -1,13 +1,14 @@
+from __future__ import division
 import spectral as sp
 import numpy as np
 import xml.dom.minidom as mdom
 from IPython import embed
 import os
+import rootpath
+import glob
+import math
 
 sep = os.path.sep
-
-def say_hi():
-    print("hi, I am hyperspec module")
 
 def read_bsq(file_path):
     path = sep.join(file_path.split(sep)[0:-1])
@@ -22,8 +23,11 @@ def read_raw(file_path):
     return img
 #-------------------------------------------------------------------------------
 
-def read_corr_matrix(file_name):
-    xml = mdom.parse(file_name)
+def rad_to_refl(img, white, dark_ti, dark_tw, ti, tw):
+    return (img - dark_ti) / (white - dark_tw) * tw / ti
+
+def read_corr_matrix(file_path):
+    xml = mdom.parse(file_path)
     virtual_bands = xml.getElementsByTagName("virtual_band")
     wavelengths = []
     coefficients = []
@@ -46,3 +50,56 @@ def read_corr_matrix(file_name):
     wavelengths = np.array(wavelengths)
 
     return wavelengths, coefficients
+#-------------------------------------------------------------------------------
+
+def read_filter_dims(file_path):
+    xml = mdom.parse(file_path)
+    height = int(str(xml.getElementsByTagName("height")[1].firstChild.data))
+    width = int(str(xml.getElementsByTagName("width")[1].firstChild.data))
+    return (height, width)
+#-------------------------------------------------------------------------------
+
+def read_offsets(file_path):
+    xml = mdom.parse(file_path)
+    offset_x = xml.getElementsByTagName("offset_x")
+    offset_y = xml.getElementsByTagName("offset_y")
+    offset_x = int(str(offset_x.item(0).firstChild.data))
+    offset_y = int(str(offset_y.item(0).firstChild.data))
+    return (offset_x, offset_y)
+#-------------------------------------------------------------------------------
+
+def read_nb_bands(file_path):
+    xml = mdom.parse(file_path)
+    bands_width = xml.getElementsByTagName("pattern_width")
+    bands_height = xml.getElementsByTagName("pattern_height")
+    bands_width = int(str(bands_width.item(0).firstChild.data))
+    bands_height = int(str(bands_height.item(0).firstChild.data))
+    return bands_height * bands_width
+#-------------------------------------------------------------------------------
+
+class HypSpecImage:
+    def __init__(self, camera):
+        self.camera = camera
+        self.xml_file = glob.glob(os.path.join(rootpath.detect(), 'cfg', camera,
+            '*.xml'))[0]
+        self.filter_dims = read_filter_dims(self.xml_file)
+        self.offset_x = read_offsets(self.xml_file)[0]
+        self.offset_y = read_offsets(self.xml_file)[1]
+        self.nb_bands = read_nb_bands(self.xml_file)
+#-------------------------------------------------------------------------------
+
+    def reshape(self, img):
+        img = img[self.offset_y:self.offset_y+self.filter_dims[0],
+            self.offset_x:self.offset_x+self.filter_dims[1]]
+        pattern_len = int(math.sqrt(self.nb_bands))
+        img_res = np.zeros((int(img.shape[0]/pattern_len),
+            int(img.shape[1]/pattern_len), self.nb_bands))
+
+        band = 0
+        for i in range(pattern_len):
+            for j in range(pattern_len):
+                img_tmp = img[np.arange(i, img.shape[0], pattern_len), :]
+                img_res[:, :, band] = img_tmp[:, np.arange(j, img.shape[1],
+                    pattern_len)]
+                band += 1
+        return img_res

23daadb43de6aded3abc44de0ab9720bb4bdab75
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-04-22 15:58:21 +0200

    Remove file

--- test_imgs.py
+++
@@ -1,9 +0,0 @@
-#!venv3/bin/python3
-
-import utils.functions as ufunc
-from IPython import embed
-
-img_path = "/media/seba/Samsung_2TB/Forest-Project/datasets/test_20200411/frames/photonfocus_nir/frame_00111.tif"
-
-img_array = ufunc.read_geotiff(img_path)
-embed()

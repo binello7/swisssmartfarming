c80231c9c23de399f987a74101c9e8153c1af44d
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-04-29 16:01:05 +0200

    Crop, merge and save merged vis_nir GeoTIFFs

--- agricircle/s_zollikofen.py
+++ agricircle/s_zollikofen.py
@@ -4,33 +4,31 @@ from datainterface import DataInterface
 import os
 import rasterio as rio
 
-from IPython import embed
 
+root_path = '/media/seba/WD_500GB/zollikofen/qgis/rasters'
 
-nir_path = ('/media/seba/WD_500GB/zollikofen/20190705/photonfocus_nir/'
-    'zollikofen_20190705_photonfocus_nir_transparent_reflectance_group1.tif')
-vis_path = ('/media/seba/WD_500GB/zollikofen/20190705/photonfocus_vis/'
-    'zollikofen_20190705_photonfocus_vis_transparent_reflectance_group1.tif')
+nir1_name = 'zollikofen_20190626_photonfocus_nir_transparent_reflectance_group1.tif'
+vis1_name = 'zollikofen_20190626_photonfocus_vis_transparent_reflectance_group1.tif'
+nir2_name = 'zollikofen_20190705_photonfocus_nir_transparent_reflectance_group1.tif'
+vis2_name = 'zollikofen_20190705_photonfocus_vis_transparent_reflectance_group1.tif'
+names = [nir1_name, vis1_name, nir2_name, vis2_name]
 
-wrong_path = ('/media/seba/WD_500GB/zollikofen/20190705/photonfocus_vis/'
-    'zollikofen_20190605_photonfocus_vis_transparent_reflectance_group1.tif')
+paths = []
+for name in names:
+    paths.append(os.path.join(root_path, name))
 
 shp_path = '/media/seba/WD_500GB/zollikofen/qgis/shapes/field.shp'
 
 
 zollikofen = DataInterface()
-zollikofen.add_dataset(nir_path)
-zollikofen.add_dataset(vis_path)
+
+for path in paths:
+    zollikofen.add_dataset(path)
 
 zollikofen.add_shapefile(shp_path)
 zollikofen.clip_with_shapefile()
 
-data, profile = zollikofen.merge_vis_nir('20190705-photonfocus_vis', '20190705-photonfocus_nir')
+merged_datasets, wavelengths = zollikofen.merge_vis_nir()
 
-filepath = '/media/seba/WD_500GB/zollikofen/20190705/new.tif'
-with rio.open(filepath, 'w', **profile) as dst:
-    dst.write(data)
-
-
-
-# embed()
+for (name, dataset), wls in zip((merged_datasets.items()), wavelengths):
+    DataInterface.write_dataset(root_path, name, dataset, wavelengths=wls)

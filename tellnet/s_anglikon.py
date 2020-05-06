6cafbf6e9b8963671c7c3f189ff493dc5423b393
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-05-06 17:32:11 +0200

    Add 'tellnet/s_anglikon.py'

---
+++ tellnet/s_anglikon.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from datainterface import DataInterface
+from glob import glob
+import os
+
+from IPython import embed
+
+
+# Input folder
+mosaics_folder = ("/media/seba/WD_500GB/tellnet/datasets/mueller/anglikon/"
+    "20200425/mosaics")
+
+# Shapefile
+shp_path = ('/media/seba/WD_500GB/tellnet/datasets/mueller/anglikon/shapes/'
+    'anglikon.shp')
+
+# Read files in mosaics_folder
+datasets_paths = glob(os.path.join(mosaics_folder, '*'))
+
+anglikon = DataInterface()
+
+# Add all dataset mosaics
+for dataset in datasets_paths:
+    anglikon.add_dataset(dataset)
+
+# Add shapefile
+anglikon.add_shapefile(shp_path)
+
+# Crop datasets with shapefile
+anglikon.crop_with_shapefile()
+
+date = '20200425'
+anglikon.set_ndvi(date)
+
+anglikon.write_ndvi(mosaics_folder, date)

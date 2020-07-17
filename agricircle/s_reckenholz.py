9b7da3fb6028b42ac46973d99a8a908055cb211f
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-07-17 17:33:02 +0200

    Apply new function 'crop_merge_write_visnir'

--- agricircle/s_reckenholz.py
+++ agricircle/s_reckenholz.py
@@ -6,12 +6,16 @@ import numpy as np
 import os
 import rasterio as rio
 
+from IPython import embed
+
 
 root_path = '/media/seba/Samsung_2TB/matterhorn-project/qgis/reckenholz'
 
 nir1_name = 'nir/20200608_flight20m_photonfocus_nir_georef.tif'
+nir2_name = 'nir/20200625_flight20m_photonfocus_nir_georef.tif'
 vis1_name = 'vis/20200608_flight20m_photonfocus_vis_georef.tif'
-names = [nir1_name, vis1_name]
+vis2_name = 'vis/20200625_flight20m_photonfocus_vis_georef.tif'
+names = [nir1_name, nir2_name, vis1_name, vis2_name]
 
 paths = []
 for name in names:
@@ -25,7 +29,6 @@ for path in paths:
     reckenholz.add_dataset(path)
 
 reckenholz.add_shapefile(shp_path)
-reckenholz.crop_with_shapefile()
 
 outputs_path = '/media/seba/Samsung_2TB/matterhorn-project/qgis/reckenholz/visnir'
-reckenholz.write_visnir(outputs_path)
+reckenholz.crop_merge_write_visnir(outputs_path)

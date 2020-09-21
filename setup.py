6f36573b20d3468fd2bfd683c24ce4c10fce7643
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-09-21 16:43:59 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='swisssmartfarming',
+    version='0.0.1',
+    description=('Package containing scripts and tools for the Swiss Smart '
+        'Farming Project.'),
+    py_modules=['functions'],
+    package_dir={'': 'utils'},
+)

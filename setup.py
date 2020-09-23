8f2e91811bb79a17492565bdb8b98dac62e8ccd7
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-09-23 17:48:46 +0200

    Update 'setup.py'

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='swisssmartfarming',
     version='0.0.1',
+    author='Sebastiano Rusca',
+    author_email='sebastiano.rusca@gmail.com',
+    url='https://github.com/binello7/swisssmartfarming',
     description=('Package containing scripts and tools for the Swiss Smart '
         'Farming Project.'),
-    py_modules=['functions'],
-    package_dir={'': 'utils'},
+    packages=find_packages(),
+    python_requires='>=3.6',
 )

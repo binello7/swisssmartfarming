510a077fce239cf97a5866b98f35ff8ac3fabbeb
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-09-29 16:19:26 +0200

    Rename file

--- brainforest/s_create_inputs.py
+++ brainforest/s_create_inputs.py
@@ -1,4 +1,5 @@
 from data_interface import Dataset, Data_Interface
+from utils import functions as ufunc
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,6 +7,11 @@ import os
 import rasterio as rio
 import rasterio.mask as riom
 import shapely
+
+from IPython import embed
+import sys
+
+sys.path.append('/home/seba/Projects/swisssmartfarming')
 
 rgb_path = ('/media/seba/Samsung_2TB/forest-project/qgis/gubler/rgb/'
             '20200626_flight2_blackfly_rgb_transparent_mosaic_group1.tif')
@@ -17,31 +23,38 @@ masks_path = ('/media/seba/Samsung_2TB/forest-project/qgis/gubler/shapes/'
 boundary_path = ('/media/seba/Samsung_2TB/forest-project/qgis/gubler/shapes/'
                  'boundary.shp')
 
-# dataset = rio.open(img_path)
+dataset = rio.open(rgb_path)
 
-# shapefile = gpd.read_file(masks_path)
-# shapes = shapefile.geometry
+shapefile = gpd.read_file(masks_path)
+shapes = shapefile.geometry
 
 # (img_mask, transf_mask) = riom.mask(dataset, shapes)
 # img_mask = np.swapaxes(img_mask, 0, 2)
 # plt.imshow(img_mask[:,:,0:3])
 
 boundary = gpd.read_file(boundary_path)
-mask = gpd.read_file(masks_path)
+tree_masks = gpd.read_file(masks_path)
 
 dataset = Dataset(
     name='gubler',
     date='20200626',
     rgb_path=rgb_path,
     ms_path=ms_path,
-    mask_shapefile=mask,
+    mask_shapefile=tree_masks,
     outer_shapefile=boundary,
     rgb_bands_to_read=[0, 1, 2],
     ms_bands_to_read=None,
     )
 dataset = [dataset]
 
-di_train = Data_Interface(dataset, {'tree': 1})
+di_train = Data_Interface(dataset, {'tree': 1, 'car': 2})
 
+img, msk = di_train.get_pair()
+# plt.imshow(msk)
 
-di_train.save('train', skip_black_greater=0.95)
+save_path = '/media/seba/Samsung_2TB/forest-project/qgis/gubler/train'
+di_train.save(save_path=save_path)
+
+# x1003_path = '/media/seba/Samsung_2TB/forest-project/qgis/gubler/train/masks/x1003_y1009.png'
+# x1003 = ufunc.read_img2array(x1003_path)
+
